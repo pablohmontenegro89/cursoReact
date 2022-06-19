19414d4e3baa7964a9ec4a51e49c66b8c7499e2c
pablohmontenegro89 <pablohmontenegro8@gmail.com>
2022-06-18 21:46:08 -0300

    First commit

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import './Card.css';
+
+function Card(){
+    return(
+        <section className="card">
+        <h4>Producto</h4>
+        <p>Descripcion</p>
+        </section>
+    );
+}
+
+export default Card;
---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+import './Welcome.css'
+
+function Welcome({name}){
+    return(
+        <h4>Bienvenido a nuestra página {name}</h4>
+    );
+}
+
+export default Welcome;
